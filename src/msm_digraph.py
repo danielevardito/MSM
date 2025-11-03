2fcf5591de1c967590488de034a079400736137b
danielevardito <danielevito.ardito@studio.unibo.it>
2025-11-03 16:03:54 +0100

    Added insert_snippet functions

--- src/msm_digraph.py
+++ src/msm_digraph.py
@@ -1,7 +1,9 @@
-from pydantic import BaseModel, Field, ValidationError
+from pydantic import BaseModel, Field, ValidationError, field_validator, model_validator
 from enum import Enum
+from typing import List
 from datetime import datetime
 from digraph import ValidatedArangoGraph
+import os
 
 class Category(str, Enum):
     """Metadata category types (not in why3 specs)."""
@@ -19,8 +21,21 @@ class Metadata(BaseModel):
 class Snippet(BaseModel):
     name: str
     content: str
-    extension: str
+    extension: str = Field(pattern=r'^[a-z]+$', max_length=10)
     created_at: datetime = Field(default_factory=datetime.now)
+
+    @field_validator('extension', mode='before')
+    @classmethod
+    def force_extension_lowercase(cls, v: str) -> str:
+        """Forza l'estensione in minuscolo prima di validarla"""
+        if isinstance(v, str):
+            return v.lower()
+        return v
+
+    @model_validator(mode='after')
+    def ensure_name_has_correct_extension(self) -> 'Snippet':
+        base_name, _ = os.path.splitext(self.name)
+        return f"{base_name}.{self.extension}"
 
 class MSMDiGraph(ValidatedArangoGraph):
     """The specific graph to use as database."""
@@ -94,6 +109,29 @@ class MSMDiGraph(ValidatedArangoGraph):
         if self.memv(new_metadata_key):
             raise ValueError("Metadata already exists")
 
-        self.insertv(metadata, new_metadata_key)
+        self.insertv(metadata, new_metadata_key, None)
         self.inserte(parent_key, new_metadata_key, RelationType.METADATA_PARENT)
+
+    def insert_metadata_list_for_snippet(self, snippet_key: str, metadata_list: List[str]):
+        if not self.is_snippet(snippet_key):
+            raise ValueError(f"Snippet {snippet_key} isn't in db")
+
+        match metadata_list:
+            case []: return
+            case [m, *l]:
+                if not self.is_metadata(m): # Precondition in why3 (forall m)
+                    raise ValueError(f"Metadata {metadata_list} isn't in db")
+                self.inserte(snippet_key, m, RelationType.HAS_METADATA)
+                self.insert_metadata_list_for_snippet(snippet_key, l)
+
+    def insert_snippet(self, snippet: Snippet, metadata_list: List[str]):
+        if self.memv(snippet.name):
+            raise ValueError(f"Snippet {snippet.name} already exists")
+        
+        self.insertv(snippet, snippet.name, "name")
+        self.insert_metadata_list_for_snippet(snippet.name, metadata_list)
+         
+    # TODO: categoria in insert_snippet
+
+
         
